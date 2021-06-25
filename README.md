2486e817718a914797ceeec5a6b9183fd7172011
jjsch-dev <juanjoseschiavoni@hotmail.com>
2021-06-25 16:58:55 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# I2C sniffer - RP2040 - PICO
+
+The purpose of this project is to develop a sniffer for the I2C BUS that can capture at 400 KHZ. For 100 Khz, optimizing the code it is possible to use by polling an 8-bit or 32-bit processor, but for 400 Khz it is necessary to react in less than 2 uS, so CPLD or FPGA are required. A middle way is to use the processor [RP2040] (https://www.raspberrypi.org/products/raspberry-pi-pico/) that has the PIO unit that, with its own ASM, executes instructions in one cycle independent of central CPUs.
+
+## I2C protocol
+
+A reduction of the i2c working principle could be that there are three conditions to detect: START  DATA and STOP and that when the level of the clock pin (SCL) is high, the transitions of the data pin (SCD) indicate start or stop, but if it remains stable is a valid data bit.
+
+![alt text](images/i2c_data_transfer.png)
+
+## Principle of operation with the PIO unit
+
+Each PIO has 4 state machines that can be programmed to decode a part of the protocol and use an irq to communicate. 
+
+For example, SM0 runs the program that decodes the START condition and triggers IRQ 7 that SM 3 listens to, which runs the main program that is waiting for the IRQs to PUSH the data in the transmission FIFO.
+
+![alt text](images/block_diagram.png)
+
+### Sniffer data encoding.
+
+When the main state machine inserts data into the FIFO, it encodes it as follows: the least significant nines are reserved for the address or data that is made up of 8 bits plus the ack / nack, and bits 11 and 12 encode the code event.
+
+![alt text](images/fifo_encode_format.png)
+
+### Connection diagram
+
+The sniffer connection to the I2C bus is as follows: the SDA line connects to the GPIO, the SCL line to the GPIO3 and the GND is shared.
+
+NOTE: the bus is 3.3V logic.
+
+![alt text](images/sniffer_diagram.png)
+
+### Test scenario 
+
+To test the capture, an arduino nano was used as a host that requests the status of a VL530 TOF every 10 mS on a 400 Khz i2c BUS..
+
+![alt text](images/test_device.png)
+
+### Preliminary results
+The following video shows the arduino monitor consulting the status, and the serial console that sends the result of the sniff of the i2c bus.
+Note: Given the nature of the test, it has not been possible to check for loss of frames or data.
+
+![](images/i2c_sniff_400khz_10mS_TOF.gif)
---
+++ main.c
@@ -0,0 +1,95 @@
+/**
+ * i2c bus sniffer pico.
+ * Sniffer for an i2c bus using the PIO of Raspberry Pi Pico (RP2040)
+ * (C) Juan Schiavoni 2021
+ *
+ * It is composed of 4 state machines that communicate through an IRQ and 
+ * two auxiliary pins. Three of them decode the START / STOP / DATA condition, 
+ * and the last one serializes the events into a single FIFO; and reads the value 
+ * of the 8-bit data plus the ACK.
+ */
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+#include "hardware/clocks.h"
+#include "i2c_sniffer.pio.h"
+
+#undef PRINT_VAL
+#define PRINT_TIME_T
+
+int main()
+{
+    const uint led_pin = 25;
+
+    // Initialize LED pin
+    gpio_init(led_pin);
+    gpio_set_dir(led_pin, GPIO_OUT);
+    gpio_put(led_pin, true);
+
+    // Full speed for the PIO clock divider
+    float div = 1;
+    PIO pio = pio0;
+
+    // Initialize chosen serial port
+    stdio_init_all();
+
+    // Initialize the four state machines that decode the i2c bus states.
+    uint sm_main = pio_claim_unused_sm(pio, true);
+    uint offset_main = pio_add_program(pio, &i2c_main_program);
+    i2c_main_program_init(pio, sm_main, offset_main, div);
+
+    uint sm_data = pio_claim_unused_sm(pio, true);
+    uint offset_data = pio_add_program(pio, &i2c_data_program);
+    i2c_stop_program_init(pio, sm_data, offset_data, div);
+
+    uint sm_start = pio_claim_unused_sm(pio, true);
+    uint offset_start = pio_add_program(pio, &i2c_start_program);
+    i2c_start_program_init(pio, sm_start, offset_start, div);
+
+    uint sm_stop = pio_claim_unused_sm(pio, true);
+    uint offset_stop = pio_add_program(pio, &i2c_stop_program);
+    i2c_stop_program_init(pio, sm_stop, offset_stop, div);
+
+    // Start running our PIO program in the state machine
+    pio_sm_set_enabled(pio, sm_main, true);
+    pio_sm_set_enabled(pio, sm_start, true);
+    pio_sm_set_enabled(pio, sm_stop, true);
+    pio_sm_set_enabled(pio, sm_data, true);
+
+    printf("i2c sniffer pico initialiced!\r\n");
+
+    // Blocks the CPU waiting for FIFO values from the main machine.
+    while (true)
+    {
+        uint32_t val = pio_sm_get_blocking(pio, sm_main);
+        
+        gpio_put(led_pin, true);
+
+        // The format of the uint32_t returned by the sniffer is composed of two event
+        // code bits (EV1 = Bit12, EV0 = Bit11), and when it comes to data, the nine least
+        // significant bits correspond to (ACK = Bit0), and the value 8 bits
+        // where (B0 = Bit1 and B7 = Bit8).
+        uint32_t ev_code = (val >> 10) & 0x03;
+        uint8_t  data = ((val >> 1) & 0xFF);
+        bool ack = (val & 1) ? false : true;
+
+#ifdef PRINT_VAL
+        printf("val: %x, ev_code: %x, data:%x, ack: %d \r\n", val, ev_code, data, ack);
+#else
+        if (ev_code == EV_START) {
+#ifdef PRINT_TIME_T
+            printf("%010d ", time_us_32());
+#endif
+            printf("s");
+        } else if (ev_code == EV_STOP) {
+            printf("o\r\n");
+        } else if (ev_code == EV_DATA) {
+            printf("%02X%c", data, ack ? 'a' : 'n');
+        } else {
+            printf("u");
+        }
+#endif
+        gpio_put(led_pin, false);
+    }
+}
